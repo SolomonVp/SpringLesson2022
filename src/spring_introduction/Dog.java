cff8243e6798ff2e59ea57e51801bdc06af28761
SolomonVP <Lum7023@yandex.ru>
2022-11-02 00:24:02 +0300

    init-destroy

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -1,14 +1,9 @@
 package spring_introduction;
 
 public class Dog implements Pet{
-    private String name;
+//    private String name;
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+
 
     public Dog() {
         System.out.println("--- Dog bean is created!");
@@ -18,4 +13,19 @@ public class Dog implements Pet{
     public void say() {
         System.out.println("Гав-Гав!");
     }
+
+    protected void init(){
+        System.out.println("Class Dog: init method");
+    }
+
+    private void destroy(){
+        System.out.println("Class Dog: destroy method");
+    }
+
+    //    public String getName() {
+//        return name;
+//    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 }
--- src/spring_introduction/Test4.java
+++ src/spring_introduction/Test4.java
@@ -5,18 +5,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
-        Dog myDog = context.getBean("myPet", Dog.class);
-        Dog yourDog = context.getBean("myPet", Dog.class);
+//        Dog myDog = context.getBean("myPet", Dog.class);
+//        Dog yourDog = context.getBean("myPet", Dog.class);
 
-        myDog.setName("Белка");
-        yourDog.setName("Стрелка");
+//        myDog.setName("Белка");
+//        yourDog.setName("Стрелка");
+//
+//        System.out.println(myDog.getName());
+//        System.out.println(yourDog.getName());
 
-        System.out.println(myDog.getName());
-        System.out.println(yourDog.getName());
-
-        System.out.println("Переменные ссылаются на один и тот же объект? " + (myDog == yourDog));
-        System.out.println(myDog);
-        System.out.println(yourDog);
+//        System.out.println("Переменные ссылаются на один и тот же объект? " + (myDog == yourDog));
+//        System.out.println(myDog);
+//        System.out.println(yourDog);
 
         context.close();
     }
