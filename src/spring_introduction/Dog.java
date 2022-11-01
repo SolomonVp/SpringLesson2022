bcea9e986d14baae496f0bbf37f93fa29f8c97e3
SolomonVP <Lum7023@yandex.ru>
2022-11-01 23:31:35 +0300

    Oбласть видимости бина

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -1,6 +1,14 @@
 package spring_introduction;
 
 public class Dog implements Pet{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Dog() {
         System.out.println("--- Dog bean is created!");
