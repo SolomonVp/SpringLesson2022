e631c8f243af449922e9fa83d114ab0fe392cd35
SolomonVP <Lum7023@yandex.ru>
2022-11-03 03:01:47 +0300

    @Scope, @PostConstruct, @PreDestroy

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -1,8 +1,13 @@
 package spring_introduction;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
+//@Scope("prototype")
 public class Dog implements Pet{
 //    private String name;
 
@@ -17,10 +22,12 @@ public class Dog implements Pet{
         System.out.println("Гав-Гав!");
     }
 
+    @PostConstruct
     protected void init(){
         System.out.println("Class Dog: init method");
     }
 
+    @PreDestroy
     private void destroy(){
         System.out.println("Class Dog: destroy method");
     }
