df649aaccc7820f9cb748b076e980a4afe513664
SolomonVP <Lum7023@yandex.ru>
2022-11-03 04:32:37 +0300

    @PropertySource для пропертей

--- src/spring_introduction/Test6.java
+++ src/spring_introduction/Test6.java
@@ -13,7 +13,10 @@ public class Test6 {
 //        cat1.say();
 
         Person person = context.getBean("personBean", Person.class);
-        Person person2 = context.getBean("personBean", Person.class);
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
+
+//        Person person2 = context.getBean("personBean", Person.class);
 //        person.callYourPet();
 
         context.close();
