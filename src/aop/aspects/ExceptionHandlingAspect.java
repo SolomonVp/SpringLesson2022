5cf6455c02761988789d989a4ce28281398a1dce
SolomonVP <Lum7023@yandex.ru>
2022-11-21 16:08:12 +0300

    Порядок выполнения Аспектов @Order

---
+++ src/aop/aspects/ExceptionHandlingAspect.java
@@ -0,0 +1,17 @@
+package aop.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Aspect
+@Order(30)
+public class ExceptionHandlingAspect {
+    @Before("aop.aspects.MyPointcuts.allGetMethods()")
+    public void beforeGetExceptionHandingAdvice() {
+        System.out.println("* beforeGetExceptionHandingAdvice: ловим/обрабатываем исключение при попытке получить книгу/журнал");
+    }
+}
