3108652df31adf0713cc973c906c7e9e0d66fd44
SolomonVP <Lum7023@yandex.ru>
2022-11-23 15:48:27 +0300

    Отношение один ко многим. UNI-DIRECTIONAL_1

---
+++ src/hibernate_test_2/Test1.java
@@ -0,0 +1,34 @@
+package hibernate_test_2;
+
+
+
+import hibernateTest.entity.Employee;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            Employee employee = new Employee("Alex", "Ivanov", "IT", 600);
+            session.beginTransaction();
+            session.save(employee);
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } catch (HibernateException e) {
+            throw new RuntimeException(e);
+        } finally {
+            factory.close();
+        }
+
+
+    }
+}
